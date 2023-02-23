f1280e8114b634909d2d12a9f36a14d41a4abda2
arkrangian <darkskylord24@gmail.com>
2023-02-23 19:54:55 +0900

    레포, 데이터소스 뼈대 작성중

--- app/src/main/java/com/djangoroid/android/hackathon/data/note/myNote/MyNoteRepository.kt
+++ app/src/main/java/com/djangoroid/android/hackathon/data/note/myNote/MyNoteRepository.kt
@@ -1,8 +1,10 @@
 package com.djangoroid.android.hackathon.data.note.myNote
 
 import com.djangoroid.android.hackathon.data.note.myNote.source.MyNoteDataSource
+import com.djangoroid.android.hackathon.util.ApiResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 data class Note(
     val title: String,
@@ -10,17 +12,32 @@ data class Note(
     val img: String
 )
 
-data class MyNote(
-    val myNotes: List<Note> = listOf()
+data class MyNoteData(
+    val myNotes: List<Note> = listOf(),
+    val isError: Boolean = false,
+    val errorMessage: String? = null,
 )
 
 class MyNoteRepository(
     private val myNoteDataSource: MyNoteDataSource
 ) {
 
-    private val _myNoteData: MutableStateFlow<MyNote> = MutableStateFlow(MyNote())
-    val myNoteData: StateFlow<MyNote> = _myNoteData
+    private val _myNoteData: MutableStateFlow<MyNoteData> = MutableStateFlow(MyNoteData())
+    val myNoteData: StateFlow<MyNoteData> = _myNoteData
 
-    suspend fun refreshMyNote()
+    suspend fun refreshMyNote() {
+        val result = myNoteDataSource.refreshMyNote()
+        when (result) {
+            is ApiResult.Success -> {
+                _myNoteData.update { it.copy(myNotes = result.data, isError = false, errorMessage = null) }
+            }
+            is ApiResult.Error -> {
+                _myNoteData.update { it.copy(isError = true, errorMessage = result.message) }
+            }
+            is ApiResult.Exception -> {
+                _myNoteData.update { it.copy(isError = true, errorMessage = "System Corruption") }
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/djangoroid/android/hackathon/data/note/myNote/source/MyNoteDataSource.kt
+++ app/src/main/java/com/djangoroid/android/hackathon/data/note/myNote/source/MyNoteDataSource.kt
@@ -1,4 +1,17 @@
 package com.djangoroid.android.hackathon.data.note.myNote.source
 
-class MyNoteDataSource {
+import com.djangoroid.android.hackathon.util.ApiResult
+import com.djangoroid.android.hackathon.util.handelApi
+
+// TODO: Move it to Network package DTO
+data class resultData(
+    val datas: List<Any>
+)
+
+class MyNoteDataSource(
+    private val djangoroidApiService: Any //Todo
+) {
+    suspend fun refreshMyNote(): ApiResult<resultData> {
+        //return //handleApi { //djangoroidApiService.basicLogin }
+    }
 }
