4b387e68918081447485069d8bc439f6adf44bb5
Gitsgwoo <sgwoo1114@gmail.com>
2023-02-25 15:49:26 +0900

    create new Note2.kt DTO

--- app/src/main/java/com/djangoroid/android/hackathon/data/note/openNote/source/OpenNoteDataSource.kt
+++ app/src/main/java/com/djangoroid/android/hackathon/data/note/openNote/source/OpenNoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.djangoroid.android.hackathon.data.note.openNote.source
 
 import com.djangoroid.android.hackathon.network.RestService
+import com.djangoroid.android.hackathon.network.dto.NoteSummary
 //import com.djangoroid.android.hackathon.network.dto.NoteSummary
 import com.djangoroid.android.hackathon.util.ApiResult
 import com.djangoroid.android.hackathon.util.handleApi
@@ -8,7 +9,7 @@ import com.djangoroid.android.hackathon.util.handleApi
 class OpenNoteDataSource(
     private val restService: RestService
 ) {
-//    suspend fun getOpenNoteList(): ApiResult<List<NoteSummary>> {
-//        return handleApi { restService.openNoteList().openNotes }
-//    }
+    suspend fun getOpenNoteList(): ApiResult<List<NoteSummary>> {
+        return handleApi { restService.openNoteList().openNotes }
+    }
 }
